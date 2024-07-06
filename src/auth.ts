8c1c88ba5696bdc910fa8ea72ab9aa2fbb81f01c
Sharm Yparraguire <sharmyparraguire@equitytransport.com.au>
2024-07-07 03:17:11 +0800

    test auth v5 edge 2

--- src/auth.ts
+++ src/auth.ts
@@ -5,6 +5,7 @@ import GitHub from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
 import Resend from "next-auth/providers/resend";
 import { db } from "./lib/db";
+import Credentials from "next-auth/providers/credentials";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   trustHost: true,
@@ -22,6 +23,43 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     Google,
     Resend({
       from: "no-reply@tutorial.codinginflow.com",
+    }),
+    Credentials({
+      name: "Credentials",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "email",
+          placeholder: "email@example.com",
+        },
+        password: { label: "Password", type: "password" },
+      },
+      authorize: async (credentials) => {
+        if (!credentials || !credentials.email || !credentials.password) {
+          return null;
+        }
+
+        const email = credentials.email as string;
+        const password = credentials.password as string;
+
+        let user: any = await db.user.findUnique({
+          where: {
+            email,
+          },
+        });
+
+        if (!user) {
+          user = await db.user.create({
+            data: {
+              email,
+            },
+          });
+        } else {
+          return user;
+        }
+
+        return user;
+      },
     }),
   ],
 });
